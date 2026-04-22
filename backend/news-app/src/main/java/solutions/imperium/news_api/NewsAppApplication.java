98da5268edbedd99926f65547b36a662878aef7c
Anouar Zerrik <an_zerrik@etu.enset-media.ac.ma>
2026-04-22 15:04:37 +0000

    chore: initialize backend spring boot with webflux

---
+++ backend/news-app/src/main/java/solutions/imperium/news_api/NewsAppApplication.java
@@ -0,0 +1,13 @@
+package solutions.imperium.news_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsAppApplication.class, args);
+    }
+
+}
---
+++ backend/news-app/src/test/java/solutions/imperium/news_api/NewsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package solutions.imperium.news_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
